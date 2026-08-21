bdf9f475365b9bc0f88db407684ffa5fb61c75d2
karukimori-wq <karukimori@gmail.com>
2026-08-21 11:44:10 +0900

    Import API metadata from latest SNS Planner artifact

---
+++ app/api-metadata.ts
@@ -0,0 +1,8 @@
+export const API_VERSION = "v1";
+
+export const PLATFORM_HEADERS = {
+  traceId: "x-trace-id",
+  correlationId: "x-correlation-id",
+  requestId: "x-request-id",
+  sourceApp: "x-source-app",
+} as const;
